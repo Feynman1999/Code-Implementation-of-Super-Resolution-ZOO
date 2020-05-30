702369bab949e24863896a9d7453648ded62fb4d
feynman1999 <chenyx.cs@gmail.com>
2020-05-30 15:15:19 +0800

    fix

--- options/base_options.py
+++ options/base_options.py
@@ -44,7 +44,7 @@ class BaseOptions():
         parser.add_argument('--load_size', type=int, default=286, help='scale images to this size')
         parser.add_argument('--crop_size', type=int, default=128, help='then crop to this size')
         parser.add_argument('--serial_batches', type=str2bool, default=False, help='if true, takes images in order to make batches, otherwise takes them randomly')
-        parser.add_argument('--num_threads', default=12, type=int, help='# threads for loading data')
+        parser.add_argument('--num_threads', default=7, type=int, help='# threads for loading data')
         parser.add_argument('--max_dataset_size', type=int, default=float("inf"), help='Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.')
         parser.add_argument('--normalize_means', type=str, default='0.5,0.5,0.5', help='normalize means in r,g,b  Please separate with comma. Set to zeros do not normalize.')
         parser.add_argument('--normalize_stds', type=str, default='1.0,1.0,1.0', help='normalize stds in r,g,b  Please separate with comma.')
