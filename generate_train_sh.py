393ba5fe4c1cdcae1bfad13ec82b40ef75acd606
feynman1999 <chenyx.cs@gmail.com>
2020-05-30 17:50:57 +0800

    fix

--- generate_train_sh.py
+++ generate_train_sh.py
@@ -19,24 +19,22 @@ content = '# ================ train shell ( {} ) ================\n'.format(time
 options = \
 """
         --dataroot          /opt/data/private/datasets/mgtv
-        --name              mgtv_mgtv1_48_32_100
+        --name              mgtv_mgtv1_add_scene
         --model             mgtv1
-        --display_freq      600
-        --print_freq        120
+        --display_freq      480
+        --print_freq        320
         --save_epoch_freq   500
         --gpu_ids           0
-        --batch_size        6
-        --suffix            05_27_00_25
+        --batch_size        16
+        --suffix            05_28_12_51
         --crop_size         256
         --imgseqlen         5
         --seed              1
         --max_consider_len  100
-        --ch1               48
-        --ch2               32
+        --scenedetect       True
         --continue_train    True
-        --load_epoch        epoch_1000
-        --epoch_count       1001
-        --scenedetect       False
+        --load_epoch        epoch_1500
+        --epoch_count       1501
 """
 
 if __name__ == '__main__':
