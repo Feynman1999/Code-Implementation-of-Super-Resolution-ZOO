3c3d758b375b92625d99e7c0421e1baa9f37f4a7
Yuxiang Chen <chenyx.cs@gmail.com>
2020-06-25 19:13:46 +0800

    fix

--- models/autoencoder_networks.py
+++ models/autoencoder_networks.py
@@ -31,12 +31,12 @@ class encoder(nn.Module):
     def __init__(self):
         super(encoder, self).__init__()
         self.model = nn.Sequential(
-            CONV(3, 16),  # [B, 16, 96, 96]
-            nn.MaxPool2d(2, stride=2),  # [B, 16, 48, 48]
-            CONV(16, 8),  # [B, 8, 48, 48]
-            nn.MaxPool2d(2, stride=2),  # [B, 8, 24, 24]
-            CONV(8, 8),  # [B, 8, 24, 24]
-            nn.MaxPool2d(2, stride=2),  # [B, 8, 12, 12]
+            CONV(3, 32),  # [B, 32, 96, 96]
+            nn.MaxPool2d(2, stride=2),  # [B, 32, 48, 48]
+            CONV(32, 16),  # [B, 16, 48, 48]
+            nn.MaxPool2d(2, stride=2),  # [B, 16, 24, 24]
+            CONV(16, 16),  # [B, 16, 24, 24]
+            nn.MaxPool2d(2, stride=2),  # [B, 16, 12, 12]
         )
 
     def forward(self, x):  # [B, 3, 96, 96]
@@ -47,19 +47,19 @@ class decoder(nn.Module):
     def __init__(self):
         super(decoder, self).__init__()
         self.model = nn.Sequential(
-            nn.ConvTranspose2d(8, 8, kernel_size=3, stride=2, padding=1, output_padding=1),
-            nn.PReLU(num_parameters=1, init=0.25),  # [B, 8, 24, 24]
-            CONV(8, 8),  # [B, 8, 24, 24]
-            nn.ConvTranspose2d(8, 8, kernel_size=3, stride=2, padding=1, output_padding=1),
-            nn.PReLU(num_parameters=1, init=0.25),  # [B, 8, 48, 48]
-            CONV(8, 8),  # [B, 8, 48, 48]
-            nn.ConvTranspose2d(8, 8, kernel_size=3, stride=2, padding=1, output_padding=1),
-            nn.PReLU(num_parameters=1, init=0.25),  # [B, 8, 96, 96]
-            CONV(8, 16),
-            CONV(16, 3),
+            nn.ConvTranspose2d(16, 16, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.PReLU(num_parameters=1, init=0.25),  # [B, 16, 24, 24]
+            CONV(16, 16),  # [B, 16, 24, 24]
+            nn.ConvTranspose2d(16, 16, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.PReLU(num_parameters=1, init=0.25),  # [B, 16, 48, 48]
+            CONV(16, 16),  # [B, 16, 48, 48]
+            nn.ConvTranspose2d(16, 16, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.PReLU(num_parameters=1, init=0.25),  # [B, 16, 96, 96]
+            CONV(16, 32),
+            CONV(32, 3),
         )
 
-    def forward(self, x):  # [B, 8, 12, 12]
+    def forward(self, x):  # [B, 16, 12, 12]
         return self.model(x)
 
 
